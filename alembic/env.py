24dfc86ae1667c534e4c8ffee0a1a8e3d2369460
Deezinn <developer.andrelz@gmail.com>
2026-05-05 22:07:08 -0300

    feat: import base to metadata import

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from database.base import Base
 from key import url
 
 # this is the Alembic Config object, which provides
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
